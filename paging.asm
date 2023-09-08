713838e27e524d4debd1d2031159553944190f6f
pitust <piotr@stelmaszek.com>
2023-09-08 13:29:44 +0100

    wip

--- paging.asm
+++ paging.asm
@@ -6,18 +6,19 @@ mapping_2m equ 0x83
   mov word [eax + 0x000], page_table | 0x2000 ; Write page table root
   mov word [0x1FF8], page_table | 0x2000 ; Upper half is same
   mov cr3, eax
-  mov word [0x2000], page_table | 0x3000 ; First G
-  mov word [0x2008], page_table | 0x4000 ; Second G
+  mov word [0x2000], page_table | 0x3000 ; +0G
+  mov word [0x2008], page_table | 0x4000 ; +1G
+  mov word [0x2010], page_table | 0x5000 ; +2G
+  mov word [0x2018], page_table | 0x6000 ; +3G
   mov word [0x2FF0], page_table | 0x3000 ; -2G
   mov word [0x2FF8], page_table | 0x4000 ; -1G
 
   ; Indentity map bottom 2G
   mov di, 0x3000
   xor ax, ax
-  mov cx, 0x400
 moremappings:
   mov byte [di], mapping_2m ; Third level
   mov word [di + 2], ax
   add di, 8
   add ax, 32
-  loop moremappings
+  jnc moremappings
